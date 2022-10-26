3796aeca645d83308955464dc40e3c73fde90423
yoxchen95 <98132024+yoxchen95@users.noreply.github.com>
2022-10-26 10:58:54 -0700

    Add test for title & subtitle

---
+++ demo.component.spec.ts
@@ -0,0 +1,147 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+// add By to query 
+import { By } from '@angular/platform-browser'
+
+import { MaterialModule } from "../material.module";
+import { DemoComponent } from "./demo.component";
+
+// ==== check create or not and defalut value ===== 
+describe("DemoComponent", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'Title'`, () => {
+    expect(component.slots.title).toEqual("Title");
+  });
+
+  it(`should have as subtitle 'SubTitle'`, () => {
+    expect(component.slots.subtitle).toEqual("Subtitle");
+  });
+
+  it(`should have as page title 'ReadAlong Studio'`, () => {
+    expect(component.getTitle()).toEqual("ReadAlong Studio");
+  });
+
+});
+
+// ================test page title=================
+// === test editable test page before edited ===
+describe("DemoComponent-pagetitle-defalutvalue", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("test editable defalut page subtitle ", () => {
+     const { debugElement } = fixture;
+     const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+     expect(title1.textContent).toContain('ReadAlong Studio');
+  });
+});
+
+// === test editable test page after edited ===
+describe("DemoComponent-pagetitle-edittvalue", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    component.setTitle("new editable Page");
+    fixture.detectChanges();
+
+  });
+
+  it("test editable page title after edit ", () => {
+    const { debugElement } = fixture;
+    const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+    expect(title1.textContent).toContain('new editable Page');
+ });
+});
+
+
+// === test editable test page after edited empty value ===
+describe("DemoComponent-pagetitle-editempty", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    component.setTitle("");
+    fixture.detectChanges();
+
+  });
+
+  it("test editable page title after edit ", () => {
+    const { debugElement } = fixture;
+    const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+    expect(title1.textContent).toBeNull;
+ });
+});
+
+// === test editable test page after twice edited ====
+describe("DemoComponent-pagetitle-edittwice", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    component.setTitle("new editable Page");
+    component.setTitle("new editable Page 2");
+    fixture.detectChanges();
+
+  });
+
+  it("test editable page title after twice edit ", () => {
+    const { debugElement } = fixture;
+    const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+    expect(title1.textContent).toContain('new editable Page 2');
+ });
+});
---
+++ demo.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+
+@Component({
+  selector: "app-demo",
+  templateUrl: "./demo.component.html",
+  styleUrls: ["./demo.component.sass"],
+})
+export class DemoComponent implements OnInit {
+  @Input() b64Inputs: string[];
+
+  slots: any = { title: "Title", subtitle: "Subtitle" };
+
+  constructor(private titleService: Title) {
+    this.setTitle("ReadAlong Studio");
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
+  public getTitle(): string {
+    return this.titleService.getTitle();
+  }
+
+  ngOnInit(): void {}
+
+  download() {
+    console.log(this.slots);
+    var element = document.createElement("a");
+    let blob = new Blob(
+      [
+        `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0">
+      <title>${this.getTitle()}</title>
+      <link rel="stylesheet" href="${this.b64Inputs[3][1]}">
+      <script src="${this.b64Inputs[3][0]}"></script>
+    </head>
+    <body>
+        <read-along text="${this.b64Inputs[1]}" alignment="${
+          this.b64Inputs[2]
+        }" audio="${this.b64Inputs[0]}" use-assets-folder="false">
+        <span slot="read-along-header">${this.slots.title}</span>
+        <span slot="read-along-subheader">${this.slots.subtitle}</span>
+        </read-along>
+    </body>
+    </html>`,
+      ],
+      { type: "text/html;charset=utf-8" }
+    );
+    element.href = window.URL.createObjectURL(blob);
+    element.download = "readalong.html";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+}
