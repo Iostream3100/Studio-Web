1a8738bcdf1411814dcbd9b2ea5f247255a6e96f
crystaljessica <13522563124@163.com>
2022-11-29 01:13:25 +0000

    feat: test for milestone3

--- src/app/demo/demo.component.spec.ts
+++ src/app/demo/demo.component.spec.ts
@@ -10,6 +10,8 @@ import {
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpEventType } from "@angular/common/http";
+import { B64Service } from "../b64.service";
 // ==== check create or not and defalut value =====
 describe("DemoComponent", () => {
   let component: DemoComponent;
@@ -93,8 +95,11 @@ describe("DemoComponent", () => {
     expect(formData.nativeElement.value).toBe("new editable Page 3");
   });
 
+  var num: number = Math.ceil(Math.random() * 10);
+  var car: string = '!"§$%&/()=?\u{20ac}';
+  var res: string = car.substring(Math.floor(car.length * Math.random()), 1);
+
   it("test editable page title after random times of edit ", () => {
-    let num: number = Math.ceil(Math.random() * 10);
     const { debugElement } = fixture;
     const formData = fixture.debugElement.query(By.css("#newpagetitle"));
     formData.nativeElement.value = num;
@@ -103,7 +108,6 @@ describe("DemoComponent", () => {
   });
 
   it("test editable page title after random times of space in the front", () => {
-    let num: number = Math.ceil(Math.random() * 10);
     const { debugElement } = fixture;
     const formData = fixture.debugElement.query(By.css("#newpagetitle"));
     formData.nativeElement.value =
@@ -115,7 +119,6 @@ describe("DemoComponent", () => {
   });
 
   it("test editable page title after random times of space in the end", () => {
-    let num: number = Math.ceil(Math.random() * 10);
     const { debugElement } = fixture;
     const formData = fixture.debugElement.query(By.css("#newpagetitle"));
     formData.nativeElement.value =
@@ -127,8 +130,6 @@ describe("DemoComponent", () => {
   });
 
   it("test editable page title including special characters", () => {
-    let car: string = '!"§$%&/()=?\u{20ac}';
-    let res: string = car.substring(Math.floor(car.length * Math.random()), 1);
     const { debugElement } = fixture;
     const formData = fixture.debugElement.query(By.css("#newpagetitle"));
     formData.nativeElement.value = "new editable Page" + res;
@@ -137,8 +138,6 @@ describe("DemoComponent", () => {
   });
 
   it("test editable page title including special characters", () => {
-    let car: string = '!"§$%&/()=?\u{20ac}';
-    let res: string = car.substring(Math.floor(car.length * Math.random()), 1);
     const { debugElement } = fixture;
     const formData = fixture.debugElement.query(By.css("#newpagetitle"));
     formData.nativeElement.value = "new editable Page" + res;
@@ -239,3 +238,104 @@ function randomRange(min: number, max: number) {
   }
   return returnStr;
 }
+
+// test for milestone3
+describe("B64Service", () => {
+  let service: B64Service;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(B64Service);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should turn utf8 to b64 and back", () => {
+    let testUTF8 = `󳬏ۓ脶򍫐䏷򻱚1󔊣򧰗J鷍ˑ⨘󝳗ʳꟴ󆋔=є򻥼Ӳ򦿴¦槩7}摠꾀𴮣۝م𬷊
+    ,^⒆!טФ񤨷Յe󫱷ъ"Ҁ*=ߋ󻅷񌍖_ᾀ\ꡝ񲁿g"՝MU񔡆ЀL
+    劆֒񦘰ˑ{坋𹔸lǼc&񓱬񊄸Ӽ:󌈅=̹ɽ渭觙􈯶൰ȣ¡圤𹷟򱄢揋ﺝ􊃻^
+    E̶򱩀򑪟eٌϐ򔜮霗燨综*􍪻񭚴oꕃ𷴨2ҽT񺆥uR혙񗊭:򉪼񙏺ۤƓ騡
+    񱺡􌡩PȌ񸛍񩍢􅓴冖㌃ۄ𦄜7Ž⇆*zѱ澁nަvۭË́듍JҢ䆹M󩗟繶`;
+    expect(service.b64_to_utf8(service.utf8_to_b64(testUTF8))).toEqual(
+      testUTF8
+    );
+  });
+
+  // the situation when there is only one word'test'
+  it("should b63service works when with one word of translation", () => {
+    let testxml = `PFRFST4KICAgIDx0ZXh0IHhtbDpsYW5nPSJlbmciIGZhbGxiYWNrLWxhbmdzPSJ1
+    bmQiIGlkPSJ0MCI+CiAgICAgICAgPGJvZHkgaWQ9InQwYjAiPgogICAgICAgICAgICA8ZGl2IHR5cGU9
+    InBhZ2UiIGlkPSJ0MGIwZDAiPgogICAgICAgICAgICAgICAgPHAgaWQ9InQwYjBkMHAwIj4KICAgICAg
+    ICAgICAgICAgICAgICA8cyBpZD0idDBiMGQwcDBzMCI+PHcgaWQ9InQwYjBkMHAwczB3MCIgQVJQQUJF
+    VD0iVCBFSCBTIFQiPnRlc3Q8L3c+IDx3IGlkPSJ0MGIwZDBwMHMwdzEiIEFSUEFCRVQ9IlQgRUggUyBU
+    Ij50ZXN0PC93Pjwvcz48cCBjbGFzcz0idHJhbnNsYXRpb24iPnpoZXNoaW48L3A+CiAgICAgICAgICAg
+    ICAgICA8L3A+CiAgICAgICAgICAgIDwvZGl2PgogICAgICAgIDwvYm9keT4KICAgIDwvdGV4dD4KPC9URUk+`;
+    let translation = service.b64_to_utf8(testxml);
+    expect(service.b64_to_utf8(service.utf8_to_b64(testxml))).toEqual(testxml);
+  });
+
+  // the situation when the sentence ocupies half of the line
+  it("should b63service works with a medium length translation", () => {
+    let testxml = `PFRFST4KICAgIDx0ZXh0IHhtbDpsYW5nPSJlbmciIGZhbGxiYWNrLWxhbmdzPSJ1bmQi
+    IGlkPSJ0MCI+CiAgICAgICAgPGJvZHkgaWQ9InQwYjAiPgogICAgICAgICAgICA8ZGl2IHR5cGU9InBhZ2Ui
+    IGlkPSJ0MGIwZDAiPgogICAgICAgICAgICAgICAgPHAgaWQ9InQwYjBkMHAwIj4KICAgICAgICAgICAgICAg
+    ICAgICA8cyBpZD0idDBiMGQwcDBzMCI+PHcgaWQ9InQwYjBkMHAwczB3MCIgQVJQQUJFVD0iREggSUggUyI+
+    dGhpczwvdz4gPHcgaWQ9InQwYjBkMHAwczB3MSIgQVJQQUJFVD0iSUggWiI+aXM8L3c+IDx3IGlkPSJ0MGIw
+    ZDBwMHMwdzIiIEFSUEFCRVQ9IkFIIj5hPC93PiA8dyBpZD0idDBiMGQwcDBzMHczIiBBUlBBQkVUPSJUIEVI
+    IFMgVCI+dGVzdDwvdz4gPHcgaWQ9InQwYjBkMHAwczB3NCIgQVJQQUJFVD0iRiBBTyBSIj5mb3I8L3c+IDx3
+    IGlkPSJ0MGIwZDBwMHMwdzUiIEFSUEFCRVQ9Ik0gQVkgTCBTIFQgT1cgTiI+bWlsZXN0b25lPC93PiA8dyBp
+    ZD0idDBiMGQwcDBzMHc2IiBBUlBBQkVUPSJHIFVIIEQiPmdvb2Q8L3c+Ljwvcz48cCBjbGFzcz0idHJhbnNs
+    YXRpb24iPui/meaYr+S4gOS4qua1i+ivlTwvcD4KICAgICAgICAgICAgICAgIDwvcD4KICAgICAgICAgICAg
+    PC9kaXY+CiAgICAgICAgPC9ib2R5PgogICAgPC90ZXh0Pgo8L1RFST4=`;
+    let translation = service.b64_to_utf8(testxml);
+    expect(service.b64_to_utf8(service.utf8_to_b64(testxml))).toEqual(testxml);
+  });
+
+  // the situation when the sentence ocupies one line
+  it("should b63service works with a medium length translation", () => {
+    let testxml = `PFRFST4KICAgIDx0ZXh0IHhtbDpsYW5nPSJlbmciIGZhbGxiYWNrLWxhbmdzPSJ1bmQiIGlkPSJ0MCI+Ci
+    AgICAgICAgPGJvZHkgaWQ9InQwYjAiPgogICAgICAgICAgICA8ZGl2IHR5cGU9InBhZ2UiIGlkPSJ0MGIwZDAiPgogICAgICAg
+    ICAgICAgICAgPHAgaWQ9InQwYjBkMHAwIj4KICAgICAgICAgICAgICAgICAgICA8cyBpZD0idDBiMGQwcDBzMCI+PHcgaWQ9In
+    QwYjBkMHAwczB3MCIgQVJQQUJFVD0iREggSUggUyI+dGhpczwvdz4gPHcgaWQ9InQwYjBkMHAwczB3MSIgQVJQQUJFVD0iSUgg
+    WiI+aXM8L3c+IDx3IGlkPSJ0MGIwZDBwMHMwdzIiIEFSUEFCRVQ9IlQgRUggUyBUIj50ZXN0PC93PiA8dyBpZD0idDBiMGQwcD
+    BzMHczIiBBUlBBQkVUPSJUIEVIIFMgVCI+dGVzdDwvdz4gPHcgaWQ9InQwYjBkMHAwczB3NCIgQVJQQUJFVD0iRiBBTyBSIj5m
+    b3I8L3c+IDx3IGlkPSJ0MGIwZDBwMHMwdzUiIEFSUEFCRVQ9IkhIIEFFIFAgSVkiPmhhcHB5PC93PiA8dyBpZD0idDBiMGQwcDB
+    zMHc2IiBlZmZlY3RpdmUtZzJwLWxhbmc9InVuZCIgQVJQQUJFVD0iSyBJWSBBQSBPVyBUIE9XIE4gRyI+eGlhb3Rvbmc8L3c+ID
+    x3IGlkPSJ0MGIwZDBwMHMwdzciIGVmZmVjdGl2ZS1nMnAtbGFuZz0idW5kIiBBUlBBQkVUPSJZIE9XIE4gRyBLIElZIEFBIE4gR
+    yI+eW9uZ3hpYW5nPC93PiA8dyBpZD0idDBiMGQwcDBzMHc4IiBlZmZlY3RpdmUtZzJwLWxhbmc9InVuZCIgQVJQQUJFVD0iRiBFW
+    SBOIEcgWSBVVyBOIj5mZW5neXVuPC93PiA8dyBpZD0idDBiMGQwcDBzMHc5IiBlZmZlY3RpdmUtZzJwLWxhbmc9InVuZCIgQVJQQ
+    UJFVD0iTSBFWSBOIEcgRCBJWSI+bWVuZ2RpPC93PiA8dyBpZD0idDBiMGQwcDBzMHcxMCIgQVJQQUJFVD0iRUggUiBJSCBLIj5lc
+    mljPC93Pjwvcz48cCBjbGFzcz0idHJhbnNsYXRpb24iPui/meaYr+S4gOS4qua1i+ivlea2jOWQkeiSmeiSgm48L3A+CiAgICAgI
+    CAgICAgICAgICA8L3A+CiAgICAgICAgICAgIDwvZGl2PgogICAgICAgIDwvYm9keT4KICAgIDwvdGV4dD4KPC9URUk+`;
+    let translation = service.b64_to_utf8(testxml);
+    expect(service.b64_to_utf8(service.utf8_to_b64(testxml))).toEqual(testxml);
+  });
+
+  // the situation when the sentence ocupies more than one line
+  it("should b63service works with a medium length translation", () => {
+    let testxml = `PFRFST4KICAgIDx0ZXh0IHhtbDpsYW5nPSJlbmciIGZhbGxiYWNrLWxhbmdzPSJ1bmQiIGlkPSJ0MCI+CiAgICAgICAgPGJvZHkgaWQ9InQwY
+    jAiPgogICAgICAgICAgICA8ZGl2IHR5cGU9InBhZ2UiIGlkPSJ0MGIwZDAiPgogICAgICAgICAgICAgICAgPHAgaWQ9InQwYjBkMHAwIj4KICAgICAgICAgICAgI
+    CAgICAgICA8cyBpZD0idDBiMGQwcDBzMCI+PHcgaWQ9InQwYjBkMHAwczB3MCIgZWZmZWN0aXZlLWcycC1sYW5nPSJ1bmQiIEFSUEFCRVQ9IkNIIEhIIE9XIE4gR
+    yBBQSI+Y2hvbmdhPC93PiA8dyBpZD0idDBiMGQwcDBzMHcxIiBlZmZlY3RpdmUtZzJwLWxhbmc9InVuZCIgQVJQQUJFVD0iQ0ggSEggT1cgTiBHIEFBIj5jaG9uZ
+    2E8L3c+IDx3IGlkPSJ0MGIwZDBwMHMwdzIiIGVmZmVjdGl2ZS1nMnAtbGFuZz0idW5kIiBBUlBBQkVUPSJDSCBISCBPVyBOIEcgQUEiPmNob25nYTwvdz4gPHcga
+    WQ9InQwYjBkMHAwczB3MyIgZWZmZWN0aXZlLWcycC1sYW5nPSJ1bmQiIEFSUEFCRVQ9IkNIIEhIIE9XIE4gRyBBQSI+Y2hvbmdhPC93PiA8dyBpZD0idDBiMGQwc
+    DBzMHc0IiBlZmZlY3RpdmUtZzJwLWxhbmc9InVuZCIgQVJQQUJFVD0iQ0ggSEggT1cgTiBHIEFBIj5jaG9uZ2E8L3c+IDx3IGlkPSJ0MGIwZDBwMHMwdzUiIGVmZ
+    mVjdGl2ZS1nMnAtbGFuZz0idW5kIiBBUlBBQkVUPSJDSCBISCBPVyBOIEcgQUEiPmNob25nYTwvdz4gPHcgaWQ9InQwYjBkMHAwczB3NiIgZWZmZWN0aXZlLWcyc
+    C1sYW5nPSJ1bmQiIEFSUEFCRVQ9IkNIIEhIIE9XIE4gRyBBQSI+Y2hvbmdhPC93PiA8dyBpZD0idDBiMGQwcDBzMHc3IiBlZmZlY3RpdmUtZzJwLWxhbmc9InVuZ
+    CIgQVJQQUJFVD0iQ0ggSEggT1cgTiBHIEFBIj5jaG9uZ2E8L3c+IDx3IGlkPSJ0MGIwZDBwMHMwdzgiIGVmZmVjdGl2ZS1nMnAtbGFuZz0idW5kIiBBUlBBQkVUP
+    SJDSCBISCBPVyBOIEcgQUEiPmNob25nYTwvdz4gPHcgaWQ9InQwYjBkMHAwczB3OSIgZWZmZWN0aXZlLWcycC1sYW5nPSJ1bmQiIEFSUEFCRVQ9IkNIIEhIIE9XI
+    E4gRyBBQSI+Y2hvbmdhPC93PiA8dyBpZD0idDBiMGQwcDBzMHcxMCIgZWZmZWN0aXZlLWcycC1sYW5nPSJ1bmQiIEFSUEFCRVQ9IkNIIEhIIE9XIE4gRyBBQSI+Y
+    2hvbmdhPC93PiA8dyBpZD0idDBiMGQwcDBzMHcxMSIgZWZmZWN0aXZlLWcycC1sYW5nPSJ1bmQiIEFSUEFCRVQ9IkNIIEhIIE9XIE4gRyBBQSI+Y2hvbmdhPC93P
+    jwvcz48cCBjbGFzcz0idHJhbnNsYXRpb24iPmNob25nYSBjaG9uZ2EgY2hvbmdhIGNob25nYWNob25nYSBjaG9uZ2EgY2hvbmdhIGNob25nYWNob25nYSBjaG9uZ
+    2EgY2hvbmdhIGNob25nYWNob25nYSBjaG9uZ2EgY2hvbmdhIGNob25nYWNob25nYSBjaG9uZ2EgY2hvbmdhIGNob25nYWNob25nYSBjaG9uZ2EgY2hvbmdhIGNob
+    25nYTwvcD4KICAgICAgICAgICAgICAgIDwvcD4KICAgICAgICAgICAgPC9kaXY+CiAgICAgICAgPC9ib2R5PgogICAgPC90ZXh0Pgo8L1RFST4=`;
+    let translation = service.b64_to_utf8(testxml);
+    expect(service.b64_to_utf8(service.utf8_to_b64(testxml))).toEqual(testxml);
+  });
+});
