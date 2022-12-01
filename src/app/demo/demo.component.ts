fb6a4748de21ddc6828144fc82cffc7734e89f6a
Yun Feng <iostream3100@gmail.com>
2022-12-01 10:50:17 -0800

    refactor: refactor code

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -11,7 +11,6 @@ export class DemoComponent implements OnInit {
   @Input() b64Inputs: string[];
 
   slots: any = { title: "Title", subtitle: "Subtitle" };
-  defaultImage = "image-for-page1.jpg";
   whiteImage = "white.png";
 
   initAddImagesButton: boolean = false;
@@ -31,7 +30,6 @@ export class DemoComponent implements OnInit {
   }
 
   updateImageInTextXML(pageIndex: number, url: string) {
-    console.log("base64[1]: ", this.b64Inputs[1]);
     const textXML = this.b64Service.b64_to_utf8(
       this.b64Inputs[1].substring(this.b64Inputs[1].indexOf(",") + 1)
     );
@@ -53,10 +51,7 @@ export class DemoComponent implements OnInit {
 
     graphic = page.querySelector("graphic");
 
-    console.log("pageIndex, url", pageIndex, url);
-
     if (url == null || url.length == 0 || url.includes(this.whiteImage)) {
-      console.log("here");
       graphic?.parentNode?.removeChild(graphic);
     } else {
       graphic?.setAttribute("url", url);
@@ -65,36 +60,28 @@ export class DemoComponent implements OnInit {
     const serializer = new XMLSerializer();
     const xmlStr = serializer.serializeToString(doc);
 
-    console.log("XML after: ", xmlStr);
-
     this.b64Inputs[1] =
       this.b64Inputs[1].slice(0, this.b64Inputs[1].indexOf(",") + 1) +
       this.b64Service.utf8_to_b64(xmlStr);
   }
 
   initImageForEachPage(): void {
-    var textXML = this.b64Service.b64_to_utf8(
+    const textXML = this.b64Service.b64_to_utf8(
       this.b64Inputs[1].substring(this.b64Inputs[1].indexOf(",") + 1)
     );
-    console.log("textXML before:", textXML);
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(textXML, "application/xml");
     const pages = doc.querySelectorAll("div[type=page]");
-    console.log("XML pages: ", pages);
     pages.forEach((page) => {
-      // page.insertAdjacentHTML("afterbegin", "<graphic url={{this.whiteImage}}/>");
       page.insertAdjacentHTML(
         "afterbegin",
         `<graphic url="${this.whiteImage}"/>`
       );
-
-      //page.querySelector(image.parentNode)
     });
 
     const serializer = new XMLSerializer();
     const xmlStr = serializer.serializeToString(doc);
-    console.log("textXML after:", xmlStr);
 
     this.b64Inputs[1] =
       this.b64Inputs[1].slice(0, this.b64Inputs[1].indexOf(",") + 1) +
@@ -111,7 +98,6 @@ export class DemoComponent implements OnInit {
       if (this.editImageMode) {
         this.displayImageContainers(true);
       } else {
-        console.log("delete graphics");
         this.displayImageContainers(false);
         this.deleteGraphicsInXML();
       }
@@ -120,7 +106,6 @@ export class DemoComponent implements OnInit {
 
   // delete all graphics in text XML
   deleteGraphicsInXML(): void {
-    console.log("inside deleteGraphicsInXML");
     // @ts-ignore
     const readalongRoot: any = document.querySelector("read-along").shadowRoot;
     const images = readalongRoot.querySelectorAll(".image");
@@ -165,15 +150,8 @@ export class DemoComponent implements OnInit {
 
       button_url.addEventListener("click", () => {
         const currURL = images[imageIndex].getAttribute("src");
-        let imgURL = prompt(
-          "Please enter image url",
-          currURL && currURL.includes(this.defaultImage) ? "" : currURL
-        );
+        let imgURL = prompt("Please enter image url", currURL ? "" : currURL);
 
-        // set image to default if user enter an empty url.
-        if (imgURL == "") {
-          imgURL = "assets/" + this.defaultImage;
-        }
         if (imgURL != null) {
           this.updateImageInHTML(imageIndex, imgURL);
           this.updateImageInTextXML(imageIndex, imgURL);
@@ -197,18 +175,12 @@ export class DemoComponent implements OnInit {
             const filereader = new FileReader();
             filereader.onloadend = () => {
               // @ts-ignore
-              // const base64result = filereader.result.substr(filereader.result.indexOf(",") + 1);
-
-              console.log("fileReader result:", filereader.result);
-              // @ts-ignore
               this.updateImageInHTML(i, filereader.result);
               // @ts-ignore
               this.updateImageInTextXML(i, filereader.result); //here we call some other functions which most likely don't cause any problems
             };
             filereader.readAsDataURL(f);
           })(e, imageIndex);
-
-          // this.picked(e);
         };
         images[imageIndex].insertAdjacentElement("beforebegin", button_delete);
 
@@ -217,52 +189,10 @@ export class DemoComponent implements OnInit {
           this.updateImageInHTML(imageIndex, imgURL);
           this.updateImageInTextXML(imageIndex, imgURL);
 
-          //this.picked(null);
-
           button_delete.remove();
         });
       });
     }
-  }
-
-  imgBase64: any = null;
-
-  //pick the image from local and update to the html
-  public picked(event: any) {
-    let fileList: FileList = event.target.files;
-    if (fileList.length > 0) {
-      const file: File = fileList[0];
-      this.handleInputChange(file); //turn into base64
-      // let url_local = URL.createObjectURL(file);
-      // this.updateImage(imageIndex, url_local);
-      // this.updateImageInTextXML(imageIndex, url_local);
-    } else {
-      console.log("No file selected");
-      console.log(fileList);
-      alert("No file selected");
-    }
-  }
-
-  handleInputChange(file: any) {
-    const pattern = /image-*/;
-    const reader = new FileReader();
-    if (!file.type.match(pattern)) {
-      alert("invalid format");
-      return;
-    }
-    reader.onloadend = this._handleReaderLoaded.bind(this);
-    reader.readAsDataURL(file);
-  }
-
-  _handleReaderLoaded(e: any) {
-    let reader = e.target;
-
-    const base64result = reader.result.substr(reader.result.indexOf(",") + 1);
-
-    this.imgBase64 = base64result;
-
-    this.updateImageInHTML(0, reader.result);
-    this.updateImageInTextXML(0, reader.result);
   }
 
   /**
@@ -291,77 +221,7 @@ export class DemoComponent implements OnInit {
     }
   }
 
-  /**
-   * remove <graphics> elements with empty or default image in text XML
-   * to avoid broken images.
-   */
-  removeEmptyImageInTextXML() {
-    const textXML = this.b64Service.b64_to_utf8(
-      this.b64Inputs[1].substring(this.b64Inputs[1].indexOf(",") + 1)
-    );
-
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(textXML, "application/xml");
-    const pages = doc.querySelectorAll("div[type=page]");
-
-    for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-      const graphic = pages[pageIndex].querySelector("graphic");
-
-      if (graphic != null) {
-        // @ts-ignore
-        const graphicURL: string = graphic.getAttribute("url");
-        console.log("graphic url: ", graphicURL);
-
-        if (graphicURL.includes(this.whiteImage) || !this.editImageMode) {
-          // @ts-ignore
-          graphic.parentNode.removeChild(graphic);
-        }
-      }
-    }
-
-    const serializer = new XMLSerializer();
-    const xmlStr = serializer.serializeToString(doc);
-
-    this.b64Inputs[1] =
-      this.b64Inputs[1].slice(0, this.b64Inputs[1].indexOf(",") + 1) +
-      this.b64Service.utf8_to_b64(xmlStr);
-  }
-
-  restoreEmptyImageInTextXML() {
-    const textXML = this.b64Service.b64_to_utf8(
-      this.b64Inputs[1].substring(this.b64Inputs[1].indexOf(",") + 1)
-    );
-
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(textXML, "application/xml");
-    const pages = doc.querySelectorAll("div[type=page]");
-
-    for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-      const graphic = pages[pageIndex].querySelector("graphic");
-
-      if (graphic == null) {
-        // pages[pageIndex].insertAdjacentHTML(
-        //   "afterbegin",
-        //   "<graphic url={{this.whiteImage}}/>"
-        // );
-        pages[pageIndex].insertAdjacentHTML(
-          "afterbegin",
-          `<graphic url="${this.whiteImage}"/>`
-        );
-      }
-    }
-
-    const serializer = new XMLSerializer();
-    const xmlStr = serializer.serializeToString(doc);
-
-    this.b64Inputs[1] =
-      this.b64Inputs[1].slice(0, this.b64Inputs[1].indexOf(",") + 1) +
-      this.b64Service.utf8_to_b64(xmlStr);
-  }
-
   download() {
-    // this.removeEmptyImageInTextXML();
-
     var element = document.createElement("a");
     let blob = new Blob(
       [
@@ -386,12 +246,9 @@ export class DemoComponent implements OnInit {
     );
 
     element.href = window.URL.createObjectURL(blob);
-    console.log("element:", element);
     element.download = "readalong.html";
     document.body.appendChild(element);
     element.click();
     document.body.removeChild(element);
-
-    // this.restoreEmptyImageInTextXML();
   }
 }
